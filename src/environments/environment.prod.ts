ab9eae648b6a0593b9998c64ff95047bba71a824
MBogushefsky <mbogushefsky@gmail.com>
2021-04-02 00:25:40 -0700

    Change production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiServer: "http://localhost:8081",
+  apiServer: "http://localhost:8080/savr-api",
   presentationMode: false,
   rainForestServer: "https://api.rainforestapi.com",
   rainForestApiKey: "238C1EE3C2F64959A27127BD9D7456D4"
